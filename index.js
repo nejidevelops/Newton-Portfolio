ac1be83954d22c4445ac537775959877eb81a0a0
Mohamed Aden Ighe <80690364+katarighe@users.noreply.github.com>
2023-06-20 12:37:25 +0300

    Add JS to the mobile popup

--- index.js
+++ index.js
@@ -26,3 +26,37 @@ mobileMenuLinks.forEach((link) => {
     document.body.style.overflow = 'auto';
   });
 });
+
+// Mobile Popup
+const projectInfos = [
+  {
+    id: 0,
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+  },
+  {
+    id: 1,
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+  },
+  {
+    id: 2,
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+  },
+  {
+    id: 3,
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+  },
+    {
+    id: 4,
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+  },
+    {
+    id: 5,
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
+  },
+];
