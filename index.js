e402cd1589134200960b9b476fdcd2e6908afedb
Mohamed Aden Ighe <80690364+katarighe@users.noreply.github.com>
2023-06-20 13:53:25 +0300

    Add cards on the JS

--- index.js
+++ index.js
@@ -60,3 +60,43 @@ const projectInfos = [
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
   },
 ];
+
+for (let i = 0; i < projectInfos.length; i += 1) {
+  const projectCard = document.createElement('div');
+  projectCard.classList.add('project-card');
+  let infoList = '';
+
+  for (let j = 0; j < projectInfos[i].info.length; j += 1) {
+    infoList += `<li>${projectInfos[i].info[j]}</li>`;
+  }
+
+  let technologiesList = '';
+  for (let k = 0; k < projectInfos[i].technologies.length; k += 1) {
+    technologiesList += `<li>${projectInfos[i].technologies[k]}</li>`;
+  }
+
+  projectCard.innerHTML = `
+      <div class="project-card">
+      <img src="${projectInfos[i].image}" class="project-card_image">
+      <div class="project-card_details">
+        <div class="stacks">
+          <h2>${projectInfos[i].title}</h2>
+        <div class="works_card_history">
+          <ul>
+            ${infoList}
+          </ul>
+        </div>
+        </div>
+        <div class="stacks">
+          <ul>
+            ${technologiesList}
+          </ul>
+        </div>
+        <button class="green-button">See project</button>
+        </div>
+      </div>
+    </div>
+  `;
+
+  card.appendChild(projectCard);
+}
